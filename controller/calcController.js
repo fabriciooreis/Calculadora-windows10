795cd7ec6552066d7ea6523b3df17b95fae42d65
fabriciooreis <fabriciooreis@gmail.com>
2020-06-10 10:39:30 -0300

    adicionando os números no vetor

--- controller/calcController.js
+++ controller/calcController.js
@@ -2,7 +2,7 @@ class calcController{
 
     constructor(){
         this._dispalyCalcEl = document.querySelector("#display");
-        
+        this._operation = [];
         this.initButtonsEventClick();
         this.initButtonsEventDrag();
         
@@ -11,14 +11,26 @@ class calcController{
         
 
     }
+    showConsole(){
+        console.log(this._operation);
+    }
+    //método para adicionar no vetor operation
+    addOperation(value){
+        this._operation.push(value);
+        this.showConsole();
+    }
+    setError(){
+        this.displayCalc = "Error";
+    }
     //iniciando o evento de click
     initButtonsEventClick(){
         let buttons = document.querySelectorAll(".row > button");
         buttons.forEach(btn=>{
             btn.addEventListener("click", e=>{
                 let txtBtn = btn.innerHTML;
+                this.excBtn(txtBtn);
                 //console.log(txtBtn+" "+e.type);
-                console.log(txtBtn);
+                //console.log(txtBtn);
             });
         });
     }
@@ -37,6 +49,25 @@ class calcController{
         return this._dispalyCalcEl;
     }
     set displayCalc(value){
-        this._dispalyCalcEl = value;
+        this._dispalyCalcEl.innerHTML = value;
+    }
+    excBtn(value){
+        switch(value){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(value);
+                break;
+            default:
+                this.setError();
+                break;
+        }
     }
 }
